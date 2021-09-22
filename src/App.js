54a088169841a643d5f531f23fbebc53f14d8f6e
chalobon <chalobon-jaje@hotmail.com>
2021-09-22 21:25:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/logo.svg';
+import heroMobile from './images/hero-mobile.jpg';
+import arrow from './images/icon-arrow.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <main>
+        <div className="content">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <img src={heroMobile} alt="" className="image_mobile" />
+
+          <h1>
+            <span>We're </span>coming soon
+          </h1>
+          <p>
+            Hello fellow shoppers! We're currently building our new fashion
+            store. Add your email below to stay up-to-date with announcements
+            and our launch deals.
+          </p>
+          <div className="email">
+            <input type="text" placeholder="Email Address" />
+            <button>
+              <img src={arrow} alt="" />
+            </button>
+          </div>
+        </div>
+
+        <div className="image_desktop"></div>
+      </main>
+
+      <footer>
+        <p className="attribution">
+          Challenge by
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by <a href="https://chalobonden.github.io/">Chalobon</a>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </footer>
     </div>
   );
 }
